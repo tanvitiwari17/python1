1146647b3376629703e548531c90c809dc39101d
tanvitiwari17 <51092839+tanvitiwari17@users.noreply.github.com>
2019-05-27 23:12:07 +0530

    a

--- file1.py
+++ file1.py
@@ -4,7 +4,14 @@ Created on Mon May 27 22:54:14 2019
 
 @author: upkt
 """
-
-x=7
-print(x)
+#chapter-2
+#print 4   #this will not work
+#
+print(4)
+x=[1,2,3]
 x
+print(x)
+y=9
+y
+type('hello')
+type(4)
