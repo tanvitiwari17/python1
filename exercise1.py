72056d8bdbe53b7e2e57bc54d53fa172c11a385c
tanvitiwari17 <51092839+tanvitiwari17@users.noreply.github.com>
2019-05-30 15:45:54 +0530

    exercises

---
+++ exercise1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 15:17:01 2019
+
+@author: upkt
+"""
+
+#programs without using functions
+
+# program for conversion of temp from C to F
+temp=eval(input("enter the temperature in celsius:"))
+print("temp in F is:",9/5*temp+32)
+
+#program to find the average of two numbers
+temp1=eval(input("enter the first number:"))
+temp2=eval(input("enter the second number:"))
+print("sum of two numbers is=",(temp1+temp2)/2)
+
+#program to find square of a number
+num=eval(input("enter the number:"))
+print("the square of",num,"is=",num*num)
