e9b7d587cc63ea84128f2d7dd79cc491c1f0e4e3
tanvitiwari17 <51092839+tanvitiwari17@users.noreply.github.com>
2019-06-14 20:42:37 +0530

    message

---
+++ file5.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+vowels={'a','e','i','a','i','o','u','o'}
+vowels
+num={'1','2','3','2','2','4','5','7','6'}
+sorted(num)
+
+
+vow=set('aeeaiouu')
+word='hello'
+u=vow.union(set(word))
+d=vow.difference(set(word))
+i=vow.intersection(set(word))
+vow
+u
+d
+i
