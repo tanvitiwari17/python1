a3a4ef2c680200bab26667f8c193ac8ab83d344f
Dhiraj Upadhyaya <dup1966@gmail.com>
2020-04-21 19:20:46 +0530

    a

--- ml1.py
+++ ml1.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
+#First File Tanvi
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sklearn.linear_model
-
+#data2b = pd.read_csv('E:/pywork/pyprojects/duanalytics/pyanalytics/mtcars.csv')
 #load the data
-oecd_bli=pd.read_csv("oecd_bli_2015.csv",thousands=',')
-gdp_per_capita=pd.read_csv("gdp_per_capita.csv",thousands=',',delimiter='\t',encoding="latin1",na_values="n/a")
-
-
+oecd_bli=pd.read_csv("E:/analytics/projects/python1/data/oecd_bli_2015.csv",thousands=',')
+gdp_per_capita=pd.read_csv('E:/analytics/projects/python1/data/gdp_per_capita3.csv')
+#, encoding='latin-1') #thousands=',', delimiter='\t', encoding="latin1", na_values="n/a")
+gdp_per_capita
+gdp_per_capita.describe
 def prepare_country_stats(oecd_bli, gdp_per_capita) :
     oecd_bli = oecd_bli[oecd_bli["INEQUALITY"]=="TOT"]
     oecd_bli = oecd_bli.pivot(index="Country", columns="Indicator", values="Value")
@@ -24,11 +20,9 @@ def prepare_country_stats(oecd_bli, gdp_per_capita) :
     full_country_stats.sort_values(by="GDP per capita", inplace=True)
     remove_indices = [0, 1, 6, 8, 33, 34, 35]
     keep_indices = list(set(range(36)) - set(remove_indices))
-    return full_country_stats[["GDP per capita", 'Life satisfaction']].iloc[keep_indices]
+    return full_country_stats[["GDP per capita", 'Life satisfaction'] ].iloc[ keep_indices]
 
 
-
-%matplotlib inline
 import matplotlib as mpl
 mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
@@ -36,7 +30,7 @@ mpl.rc('ytick', labelsize=12)
 
 
 #prepare the data
-country_stats =prepare_country_stats(oecd_bli,gdp_per_capita)
+country_stats =prepare_country_stats(oecd_bli, gdp_per_capita)
 X=np.c_[country_stats['GDP per capita']]
 Y=np.c_[country_stats["Life Satisfaction"]]
 
